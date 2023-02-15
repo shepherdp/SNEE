3419062157508d25805672e1b61ab3a8fc5f253d
shepherdp <shepherd.berea@gmail.com>
2023-02-15 00:27:32 -0500

    Cleaned up initialization; added test suite stubs

---
+++ test.py
@@ -0,0 +1,62 @@
+# Test suite for SocialNetwork.py
+
+from SocialNetwork import SocialNetwork
+def test_1_01():
+    pass
+
+def test_1_02():
+    pass
+
+def test_1_03():
+    pass
+
+def test_1_04():
+    pass
+
+def test_1_05():
+    pass
+
+def test_1_06():
+    pass
+
+def test_1_07():
+    pass
+
+def test_1_08():
+    pass
+
+def test_1_09():
+    pass
+
+def test_1_10():
+    pass
+
+def test_1_11():
+    pass
+
+def test_1_12():
+    pass
+
+def test_1_13():
+    pass
+
+def test_1_14():
+    pass
+
+def test_1_15():
+    pass
+
+def test_1_16():
+    pass
+
+def test_1_17():
+    pass
+
+def test_1_18():
+    pass
+
+def test_1_19():
+    pass
+
+def test_1_20():
+    pass
